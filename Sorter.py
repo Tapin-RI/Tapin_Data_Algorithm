5dafdea61f71e3b9a3139f925ba545260666f467
Landon Montecalvo <landonmontecalvo290@gmail.com>
2023-11-01 22:30:29 -0400

    Update Sorter.py

--- Sorter.py
+++ Sorter.py
@@ -42,23 +42,64 @@ tapinCategoriesList = {
     "33": "TEFAP Produce"
 }
 
+# Temporary Storage Lists
 dataRows = []
 tapinCategories = []
+productRefs = []
+productTypes = []
+productCategories = []
+productWeights = []
+productServiceFees = []
+productPurchaseCosts = []
+productValues = []
+
+totalWeight = 0
+totalServiceFee = 0
+totalPurchaseCost = 0
+totalValue = 0
 
 files = glob.glob('*.csv') # Return all files with the .csv extension.
+
+def RemoveCharacters(number): # Function to remove the comma and the $ from a number.
+    number = number.replace(",", "")
+    number = number.replace("$", "")
+    return number
 
 with open(files[0], 'r') as file: # Read data in the .csv file.
     reader = csv.reader(file)
 
     for index, value in enumerate(reader):
         if index > 2: # Removes header rows
-            if isinstance(value[0], str): # Remove interger rows
-                if value[0] != "Total Weight" and value[0] != "Service Fee" and value[0] != "Tap-In 113700" and value[0] != "Total \nWeight" and value[0] != " Grand Totals: " and value[0] != "Page -1 of 1": # Get rid of junk rows.
+            if value[0] != "Total Weight" and value[0] != "Service Fee" and value[0] != "Tap-In 113700" and value[0] != "Total \nWeight" and value[0] != " Grand Totals: " and value[0] != "Page -1 of 1": # Get rid of junk rows.
+                if (not RemoveCharacters(value[0]).isdigit()): # Remove number rows.
                     dataRows.append(value) # Add item row to dataRows list.
 
-    for index, value in enumerate(dataRows):
+    for index, value in enumerate(dataRows): # Lookup tapin category for each item and append it to tapinCategories.
         tempSplitString = value[2].split(" ")
-        tapinCategories.append(tempSplitString[0])
-        
-    for item in tapinCategories:
+        tapinCategories.append(tapinCategoriesList.get(tempSplitString[0]))
+
+    for item in dataRows:
         print(item)
+
+    for index, value in enumerate(dataRows):
+        # Add corresponding data from csv to one of the lists above.
+        productRefs.append(value[0])
+        productTypes.append(value[2]) 
+        productCategories.append(value[3])
+        productWeights.append(value[4])
+        productServiceFees.append(value[5])
+        productPurchaseCosts.append(value[6])
+        productValues.append(value[7])
+
+    for item in productWeights:
+        print(item)
+
+    # Generate subtotal for number columns.
+    for weight in productWeights:
+        totalWeight += int(weight)
+    for serviceFee in productServiceFees:
+        totalServiceFee += float(RemoveCharacters(serviceFee))
+    for purchaseCost in productPurchaseCosts:
+        totalPurchaseCost += float(RemoveCharacters(purchaseCost))
+    for value in productValues:
+        totalValue += float(RemoveCharacters(value))
